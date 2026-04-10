03307d7aabaa03f80c710a563170de89dee161b4
karasidnez <karasdnez@gmail.com>
2026-04-10 16:10:00 +0300

    Step 7: returning JSON from routes

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -1,6 +1,46 @@
+using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
+using System.Globalization;
+using System.IO.Pipes;
+using System.Linq.Expressions;
+using System.Net.Security;
+using System.Reflection.Metadata;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+using System.Security.Principal;
+using System.Text.RegularExpressions;
+using System.Xml.Schema;
+using Microsoft.VisualBasic;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Привет от ИСП-233! Aвтор:Тимонини И.В");
+app.MapGet("/", () => "Добро пожаловать на сервер!");
+app.MapGet("/about", () => "Это мой первый ASP.NET Core сервер");
+app.MapGet("/time", () => $"Время на сервере: {DateTime.Now}");
+app.MapGet("/hello/{name}", (string name) => $"Привет,{name}");
+app.MapGet("/sum/{a}/{b}", (int a, int b) => $"{a + b}");
+app.MapGet("/student", () => new
+{
+    Name = "Иван Тимонин",
+    Group = "ИСП-233",
+    Year = 3,
+    IsActive = true
+});
+app.MapGet("/subjects", () => new[]
+{
+    "РПМ",
+    "РМП",
+    "ИСРПО",
+    "СП",
+});
+app.MapGet("/product/{id}", (int id) => new Product(
+    Id: id,
+    Name: $"Товар #{id}",
+    Price: id * 99.99m,
+    InStock: id % 2 == 0
+));
+
 
 app.Run();
+record Product(int Id, string Name, decimal Price, bool InStock);
